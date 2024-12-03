f2be11aa57065e249be6557a1e97b694752e4249
SthephannyLorrany <sthephannyvilas445@gmail.com>
2024-12-03 09:55:30 -0300

    Add files via upload

---
+++ Aula 2/Aula02.py
@@ -0,0 +1,11 @@
+print("to com fome")
+
+nome_usuario ='Sthephanny lorrany' #tipo -> str = string
+idade_usuario = 16 # tipo -> int = integer
+altura_usuario = 1.60 # tipo -> float = ponto flutuante
+maior_idade = False # tipo -> bool = boolean 
+
+print('Seja bem-vindo', nome_usuario)
+print('sua idade é', idade_usuario, 'anos')
+print('sua altura é', altura_usuario , 'metros')
+print('você é maior de idade?', maior_idade) 
